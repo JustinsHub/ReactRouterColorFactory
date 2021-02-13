cb6288d3b880e7dea5cc2f369c08afe89dc04bfa
Justin Z <contactjustinzamora@gmail.com>
2021-02-12 19:23:49 -0800

    added simple logic to Color component

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import {useParams, useHistory} from 'react-router-dom'
 
 const Color = () => {
+    const {color} = useParams()
+    const history = useHistory()
+
+    const handleClick = () => {
+        history.push('/colors')
+    }
     return (
-        <div>
-            Hello World
+        <div style={{backgroundColor: color}}>
+            <h1>Welcome to the color {color}!</h1>
+            <p1>Take a quick look around and feast your eyes.</p1>
+            <div>
+            <button onClick={handleClick}>Go Back To Colors</button>
+            </div>
         </div>
     )
 }
